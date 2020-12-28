52558fbd49df8da4d9cc383e99d69ffb30ca2843
itsasy <45985585+itsasy@users.noreply.github.com>
2020-12-27 19:14:15 -0500

    Add navbar in template

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavId"
+            aria-controls="collapsibleNavId" aria-expanded="false">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+
+            <ul class="navbar-nav ml-auto">
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Login</a>
+                </li>
+                @endguest
+
+                @auth
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{ auth()->user()->name }}
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">
+                            Cerrar sesión
+                        </a>
+                    </div>
+                </li>
+                @endauth
+
+            </ul>
+        </div>
+    </div>
+</nav>
